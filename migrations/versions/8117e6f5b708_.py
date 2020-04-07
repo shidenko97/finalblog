2709deb0e3c6a39b677c6bfb1b3c4d36c5b6688a
Sergey Hidenko <shidenko97@gmail.com>
2020-04-07 18:13:47 +0300

    Added `ondelete` constraint for user role links

--- migrations/versions/8117e6f5b708_.py
+++ migrations/versions/8117e6f5b708_.py
@@ -38,8 +38,8 @@ def upgrade():
     op.create_table('user_role',
     sa.Column('user_id', sa.Integer(), nullable=True),
     sa.Column('role_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['role_id'], ['role.id'], ),
-    sa.ForeignKeyConstraint(['user_id'], ['user.id'], )
+    sa.ForeignKeyConstraint(['role_id'], ['role.id'], ondelete='CASCADE', ),
+    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE', )
     )
     # ### end Alembic commands ###
 
